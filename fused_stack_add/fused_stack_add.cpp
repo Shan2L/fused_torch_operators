c90cd57265b12bf1f5d547d469b11f045c861f19
Shan2L <liushanlin720@163.com>
2024-08-02 13:57:07 +0000

    fix: remove contiguous check for input tensors

--- fused_stack_add/fused_stack_add.cpp
+++ fused_stack_add/fused_stack_add.cpp
@@ -34,17 +34,17 @@ torch::Tensor fused_stack_add(std::vector<torch::Tensor>& inputs, torch::Tensor&
     TORCH_INTERNAL_ASSERT(std::abs(dim) < ndim, "dim shoud not be greater than the dim of input tensor.");
     TORCH_INTERNAL_ASSERT(ndim<=4, "for better performance, fused concat-embedding op only support dimension <= 4.");
     dim = get_unwrapper_dim(dim, ndim);
-    bool contiguous = true;
+    // bool contiguous = true;
     bool same_dim = true;
     for (auto input: inputs)
     {
-        contiguous = input.is_contiguous() && contiguous;
+        // contiguous = input.is_contiguous() && contiguous;
         if (input.dim() != ndim)
         {
             same_dim = false;
         }
     }
-    TORCH_INTERNAL_ASSERT(contiguous == true, "fused cat-embedding op only support contiguous inputs now.");
+    // TORCH_INTERNAL_ASSERT(contiguous == true, "fused cat-embedding op only support contiguous inputs now.");
     TORCH_INTERNAL_ASSERT(same_dim == true, "input tensors must have same dimension,");
 
     bool illegal_shape = false;
