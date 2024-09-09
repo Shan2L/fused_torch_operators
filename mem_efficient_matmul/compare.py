389fbf306962d4d0d9fee09c8a24909491dc36dc
Shan2L <liushanlin720@163.com>
2024-09-09 09:00:34 +0000

    feat: add compare script for memory_efficient_mamtul

--- mem_efficient_matmul/compare.py
+++ mem_efficient_matmul/compare.py
@@ -4,11 +4,15 @@ import memory_efficient_matmul
 a = torch.randn(512, 512).float()
 b = torch.randn(512, 512).float()
 
-cpu_result = torch.mm(a, b)
+# cpu_result = torch.mm(a, b)
 
-golden = torch.mm(a.cuda(), b.cuda()).cpu()
+golden = torch.mm(a.cuda(), b.cuda())
 
-result = memory_efficient_matmul.mm(a, b, 1, 1, 128)
+result = memory_efficient_matmul.mm(a, b, 1, 1, 512)
 
-equal = torch.isclose(golden, result, 1e-4, 1e-4)
-print(torch.all(equal))
+print(result.device)
+
+# print(golden - result)
+
+# equal = torch.isclose(golden, result, 1e-5, 1e-5)
+# print(torch.all(equal))
