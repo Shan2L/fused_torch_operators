2b12f560e74403085dad8f1de4352d103e8f0400
Shan2L <liushanlin720@163.com>
2024-09-22 22:32:02 +0800

    draft: implementation of torch operator of memory efficient matmul.

---
+++ mem_efficient_matmul/cublas/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import torch
+
+setup(
+    name='memory_efficient_matmul',
+    ext_modules=[
+        CUDAExtension(
+            name='memory_efficient_matmul',
+            sources=[
+                'mem_efficient_matmul.cu',
+             ],
+            include_dirs=["/usr/local/cuda/include"],
+            library_dirs=["/usr/local/cuda/lib64"]
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
